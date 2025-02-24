32648fae3e9e2c190b4c8f12abaf42337a9f9f24
zlooper23 <noel.tuset@gmail.com>
2025-02-24 16:39:03 +0100

    gjl

--- CGFrameworkStudent/res/shaders/Tx6.fs
+++ CGFrameworkStudent/res/shaders/Tx6.fs
@@ -8,8 +8,7 @@ uniform mat4 u_projviewmatrix;
 
 void main()
 {  
-    vec2 i = vec2(1280, 720)/5.0;
-    vec2 blur = 1.0/i;
+    vec2 blur = vec2(0.003);
     vec4 col = vec4(0.0, 0.0, 0.0, 1.0);
     col += texture2D( u_texture, vec2(v_uv.x, v_uv.y));
     col += texture2D( u_texture, vec2(v_uv.x+blur.x, v_uv.y));
@@ -21,8 +20,6 @@ void main()
     col += texture2D( u_texture, vec2(v_uv.x+blur.x, v_uv.y-blur.y));
     col += texture2D( u_texture, vec2(v_uv.x-blur.x, v_uv.y+blur.y));
     col /= 9.0;
-
-
 
     gl_FragColor = vec4(col.xyz, 1.0);
 }
