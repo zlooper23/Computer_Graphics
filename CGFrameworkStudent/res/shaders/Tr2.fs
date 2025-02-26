7e2c5af2c766e69d78957e6c4246e99d52bdeb8a
zlooper23 <noel.tuset@gmail.com>
2025-02-26 18:57:09 +0100

    gh

--- CGFrameworkStudent/res/shaders/Tr2.fs
+++ CGFrameworkStudent/res/shaders/Tr2.fs
@@ -9,7 +9,7 @@ uniform vec2 u_size;
 void main()
 {  
     vec2 size = u_size/40.0;
-    float Pixels = 2048.0;
+    float Pixels = 2048.0*abs(sin(u_time*0.1));
     float dx = size.x * (1.0/Pixels);
     float dy = size.y * (1.0/Pixels);
     vec2 Coord = vec2(dx * floor(v_uv.x / dx), dy * floor(v_uv.y / dy));
